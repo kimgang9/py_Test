f2d9cb18c3304a8ba965cee20b6e649da9ac9366
kimgang9 <127107835+kimgang9@users.noreply.github.com>
2023-07-07 18:12:10 +0900

    Test_02
    사용자로부터 두 개의 숫자를 입력 받은 후 두 개의 숫자를 더한 값, 곱한 값, 나눈 값, 나눈 몫, 나머지 값을 각각 출력하는 프로그램을 작성하세요

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-user1 = int(input("밑 : "))
-user2 = int(input("지수 : "))
+user_input1 = float(input("첫번째 값: "))
+user_input2 = float(input("두번째 값: "))
 
-output = user1 ** user2
-print("답은 : {}".format(output))
+print("더한 값 => ", user_input1 + user_input2)
+print("곱한 값 => ", user_input1 * user_input2)
+print("나눈 값 => ", user_input1 / user_input2)
+print("나눈 몫 => ", user_input1 // user_input2)
+print("나머지 값 => ", user_input1 % user_input2)
